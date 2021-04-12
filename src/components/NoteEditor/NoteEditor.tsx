a0304498a0ff07a9af85a30853fd57ecf55ad3a2
Ripal Barot <ripplejb@hotmail.com>
2021-04-12 00:53:29 -0500

    Added some react best practices.

--- src/components/NoteEditor/NoteEditor.tsx
+++ src/components/NoteEditor/NoteEditor.tsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import {FC} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {Card, CardContent, TextField, Typography} from "@material-ui/core";
-import './NoteEditor.css'
+import './NoteEditorStyles'
 import {SELECT_NOTE} from "../../redux/store/actionTypes";
+import {cardContainer, cardStyle} from "./NoteEditorStyles";
 
 type Props = {
   id: number
@@ -14,50 +15,45 @@ const NoteEditor: FC<Props> = (props) => {
   const ERROR = 'ERROR'
   const NOTE_PLACEHOLDER = 'Type note here.'
   const VIEW_PLACEHOLDER = 'Click here to start taking note.'
-  const EmptyNote = () => {
-    return {id: -2, note: "", title: "", selected: false}
+
+  const getNote = (id: number, state: NoteState) => state.notes.find(n => n.id === id) ?? {
+    id: -2,
+    note: ERROR,
+    title: "",
+    selected: false
   }
 
-  const getNode = (id: number, state: NoteState) => {
-    let foundNode = state.notes.find(n => n.id === id)
-    if (foundNode === undefined) {
-      foundNode = EmptyNote();
-      foundNode.note = ERROR
-    }
-    return foundNode
-  }
-
-  const currentNote = useSelector<NoteState, INote>(state => getNode(props.id, state))
+  const currentNote = useSelector<NoteState, INote>(state => getNote(props.id, state))
 
   const dispatch = useDispatch()
 
   const isSelected = useSelector<NoteState, boolean | undefined>(state => {
-    let foundNode = getNode(currentNote.id, state);
-    return (foundNode.id === -2) ? false : foundNode.selected;
+    const foundNote = getNote(currentNote.id, state);
+    return (foundNote.id === -2) ? false : foundNote.selected;
   })
 
-  const handleNoteChange = (e: { target: { value: string; }; }) => {
-    currentNote.note = e.target.value;
-  }
-
-  const editField = () => <TextField multiline
-                                     placeholder={NOTE_PLACEHOLDER}
-                                     rows={10}
-                                     onChange={handleNoteChange}
-                                     defaultValue={currentNote.note}
-  />
-
-  const viewField = () => <Typography variant='body2' color='textSecondary' component='p' >
-    {currentNote.note === '' ? VIEW_PLACEHOLDER : currentNote.note}
-  </Typography>
-
-  const cardContent = () => isSelected ? editField() : viewField()
+  const handleNoteChange = (e: { target: { value: string; }; }) => currentNote.note = e.target.value;
 
   return (
-    <div className='card-container'>
-      <Card onClick={() => {dispatch({type: SELECT_NOTE, note: currentNote})}}>
+    <div style={cardContainer}>
+      <Card onClick={() => {
+        dispatch({type: SELECT_NOTE, note: currentNote})
+      }} style={cardStyle}>
         <CardContent>
-          {cardContent()}
+          {
+            isSelected
+              ?
+              <TextField multiline
+                         placeholder={NOTE_PLACEHOLDER}
+                         rows={10}
+                         onChange={handleNoteChange}
+                         defaultValue={currentNote.note}
+              />
+              :
+              <Typography variant='body2' color='textSecondary' component='p'>
+                {currentNote.note === '' ? VIEW_PLACEHOLDER : currentNote.note}
+              </Typography>
+          }
         </CardContent>
       </Card>
     </div>
---
+++ src/components/NoteEditor/NoteEditorStyles.tsx
@@ -0,0 +1,10 @@
+export const cardContainer = {
+  margin: '5px'
+}
+
+export const cardStyle = {
+  display: 'block',
+  width: '2.5in',
+  transitionDuration: '10.3s',
+  height: '2.5in'
+}
