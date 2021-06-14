1b311f82f7648e16f7b7d1cc57840ab25f8537aa
Ripal Barot <ripplejb@hotmail.com>
2021-06-14 05:50:58 -0500

    Added Login component unit test.

---
+++ src/tests/Login.test.tsx
@@ -0,0 +1,14 @@
+import {render} from "@testing-library/react";
+import {Provider} from "react-redux";
+import {testStore} from "./fixtures/redux-store";
+import React from "react";
+import Login from "../components/Login/Login";
+
+describe('render login screen.', () => {
+  it('should render the login screen with a username ' +
+    'and password text fields', () => {
+    const {container} = render(<Provider store={testStore}><Login/></Provider>)
+    expect(container.innerHTML).toContain('eMail');
+    expect(container.innerHTML).toContain('Password');
+  })
+})
